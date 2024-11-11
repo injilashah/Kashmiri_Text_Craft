95a015a3fe0acd0f9bd222a00c146eaa6ee94910
Injila <110339290+injilashah@users.noreply.github.com>
2024-11-11 17:34:41 +0530

    Update __init__.py

--- kashmiri/normalization/__init__.py
+++ kashmiri/normalization/__init__.py
@@ -1,5 +1,5 @@
 
 from .character import normalize_characters, normalize_combine_characters, \
-    remove_diacritics, replace_digits, normalize
+     replace_digits, normalize
 
-__all__ = ["normalize", "normalize_characters", "normalize_combine_characters", "remove_diacritics", "replace_digits"]
+__all__ = ["normalize", "normalize_characters", "normalize_combine_characters", "replace_digits"]
