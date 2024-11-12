ee2058a508aaa963e9c5f91a54cd14717fad9bdf
Injila <injilashah000@gmail.com>
2024-11-12 14:26:30 +0530

    deleted some files

--- kashmiri/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding: utf8
-"""Project Entry point"""
-from .about import __version__, get_info
-from .conll import CoNLL
-from .normalization import normalize
-from .pipeline import Pipeline
-from .utils.resources import download
-
-__all__ = ["__version__", "get_info", "normalize", "download", "CoNLL", "Pipeline"]
