d682b771b90f70c6f1600c8e28aaac07a3fffaaf
Injila <110339290+injilashah@users.noreply.github.com>
2024-11-13 14:07:32 +0530

    Update about.py

--- about.py
+++ about.py
@@ -4,11 +4,11 @@ import platform
 from pathlib import Path
 from typing import Dict
 
-__version__ = '1.0.0'  # Adjust the version number accordingly
+__version__ = '1.0.0' 
 __description__ = 'Natural Language Processing (NLP) library for Kashmiri language.'
-__url__ = 'https://github.com/injilashah/Kashmiri_normalizer.git'  # Replace with your repo URL
-__author__ = 'INJILA SHAH'  # Replace with your name
-__author_email__ = 'shahinjila435@gmail.com'  # Replace with your email
+__url__ = 'https://github.com/injilashah/Kashmiri_normalizer.git'  
+__author__ = 'INJILA SHAH' 
+__author_email__ = 'shahinjila435@gmail.com'  
 
 
 def get_info() -> Dict[str, str]:
